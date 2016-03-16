71fe9ef774b37342eeac722ffd49d6443a3cd5ab
dargaCode <darga.code@gmail.com>
2016-03-15 23:19:38 -0700

    Replace individual swatch assignments with loop

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-var swatch0 = document.querySelectorAll(".swatch")[0];
-var swatch1 = document.querySelectorAll(".swatch")[1];
-var swatch2 = document.querySelectorAll(".swatch")[2];
-var swatch3 = document.querySelectorAll(".swatch")[3];
-var swatch4 = document.querySelectorAll(".swatch")[4];
-var swatch5 = document.querySelectorAll(".swatch")[5];
+var colors = [
+  "rgb(255,000,000)",
+  "rgb(255,255,000)",
+  "rgb(255,000,255)",
+  "rgb(000,255,000)",
+  "rgb(000,255,255)",
+  "rgb(000,000,255)"
+]
 
-swatch0.style.backgroundColor = "red";
-swatch1.style.backgroundColor = "orange";
-swatch2.style.backgroundColor = "yellow";
-swatch3.style.backgroundColor = "green";
-swatch4.style.backgroundColor = "blue";
-swatch5.style.backgroundColor = "indigo";
+var swatches = document.querySelectorAll(".swatch");
+
+for (var i = 0; i < swatches.length; i++) {
+  swatches[i].style.backgroundColor = colors[i];
+}
