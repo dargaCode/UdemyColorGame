54d1d14be75fe90f6fa89b017e78de9d4a1df146
dargaCode <darga.code@gmail.com>
2016-03-15 23:09:03 -0700

    Add selectors for each swatch

---
+++ script.js
@@ -0,0 +1,13 @@
+var swatch0 = document.querySelectorAll(".swatch")[0];
+var swatch1 = document.querySelectorAll(".swatch")[1];
+var swatch2 = document.querySelectorAll(".swatch")[2];
+var swatch3 = document.querySelectorAll(".swatch")[3];
+var swatch4 = document.querySelectorAll(".swatch")[4];
+var swatch5 = document.querySelectorAll(".swatch")[5];
+
+swatch0.style.backgroundColor = "red";
+swatch1.style.backgroundColor = "orange";
+swatch2.style.backgroundColor = "yellow";
+swatch3.style.backgroundColor = "green";
+swatch4.style.backgroundColor = "blue";
+swatch5.style.backgroundColor = "indigo";
